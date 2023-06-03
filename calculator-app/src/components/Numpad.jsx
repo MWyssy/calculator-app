9330feec879a2866fe8038b14336125629794291
MWyssy <mike@wyss.co.uk>
2023-06-03 19:03:40 +0300

    add button functionality and started to work on the calculator logic

--- calculator-app/src/components/Numpad.jsx
+++ calculator-app/src/components/Numpad.jsx
@@ -3,37 +3,109 @@ import Screen from './Screen.jsx'
 import './Numpad.css'
 
 function Numpad() {
-    const [input, setInput] = useState('')
+    const [input, setInput] = useState({
+        number: '',
+        operator: '',
+        reset: false
+    })
 
-    function handleClick(event) {
-        setInput(event.target.value)
+    const handleClick = event => {
+        const button = event.target.outerText
+        if (button === 'C') {
+            setInput({
+                number: '',
+                operator: '',
+                reset: true
+            })
+        } else {
+            setInput({
+                number: input.number + event.target.outerText,
+                operator: '',
+                reset: false
+            })
+        }
+        if (input.number[input.number.length - 1] === ' ') {
+            setInput({
+                number: '',
+                operator: '',
+                reset: false
+            })
+        }
     }
-    
-    console.log(input)
+
+    const handleOperator = event => {
+        const button = event.target.outerText
+        if (button === '±') {
+            setInput({
+                number: '-' + input.number,
+                operator: '',
+                reset: false
+            })
+        } else if (button === '%') {
+            setInput({
+                number: Number(input.number) * 0.01.toString(),
+                operator: '',
+                reset: false
+            })
+        } else if (button === '.') {
+            if (!input.number.includes('.')) {
+                setInput({
+                    number: input.number + '.',
+                    operator: '',
+                    reset: false
+                })
+            }
+        } else if (button === '÷') {
+            setInput({
+                number: input.number + ' ÷ ',
+                operator: '÷',
+                reset: false
+            })
+        } else if (button === 'x') {
+            setInput({
+                number: input.number + ' x ',
+                operator: 'x',
+                reset: false
+            })
+        } else if (button === '-') {
+            setInput({
+                number: input.number + ' - ',
+                operator: '-',
+                reset: false
+            })
+        } else if (button === '+') {
+            setInput({
+                number: input.number + ' + ',
+                operator: '+',
+                reset: false
+            })
+        } 
+        
+    }    
 
     return (
         <>
-            <Screen />
+            <Screen input={input}/>
             <form id="numpad">
-                <button className="numpad-button symbol" id="cee" type='submit' onSubmit={handleClick}>C</button>
-                <button className="numpad-button symbol" onClick={handleClick}>±</button>
-                <button className="numpad-button symbol" onClick={handleClick}>%</button>
-                <button className="numpad-button symbol" onClick={handleClick}>÷</button>
-                <button className="numpad-button" onClick={handleClick}>7</button>
-                <button className="numpad-button" onClick={handleClick}>8</button>
-                <button className="numpad-button" onClick={handleClick}>9</button>
-                <button className="numpad-button symbol" onClick={handleClick}>x</button>
-                <button className="numpad-button" onClick={handleClick}>4</button>
-                <button className="numpad-button" onClick={handleClick}>5</button>
-                <button className="numpad-button" onClick={handleClick}>6</button>
-                <button className="numpad-button symbol" onClick={handleClick}>-</button>
-                <button className="numpad-button" onClick={handleClick}>1</button>
-                <button className="numpad-button" onClick={handleClick}>2</button>
-                <button className="numpad-button" onClick={handleClick}>3</button>
-                <button className="numpad-button symbol" onClick={handleClick}>+</button>
-                <button className="numpad-button" id="zero" onClick={handleClick}>0</button>
-                <button className="numpad-button symbol" onClick={handleClick}>.</button>
-                <button className="numpad-button symbol" onClick={handleClick}>=</button>
+                <button className="numpad-button symbol" id="cee" type='button' onClick={handleClick}>C</button>
+                <button className="numpad-button symbol" onClick={handleOperator} type='button'>±</button>
+                <button className="numpad-button symbol" onClick={handleOperator} type='button'>%</button>
+                <button className="numpad-button symbol" onClick={handleOperator} type='button'>÷</button>
+                <button className="numpad-button" onClick={handleClick} type='button'>7</button>
+                <button className="numpad-button" onClick={handleClick} type='button'>8</button>
+                <button className="numpad-button" onClick={handleClick} type='button'>9</button>
+                <button className="numpad-button symbol" onClick={handleOperator} type='button'>x</button>
+                <button className="numpad-button" onClick={handleClick} type='button'>4</button>
+                <button className="numpad-button" onClick={handleClick} type='button'>5</button>
+                <button className="numpad-button" onClick={handleClick} type='button'>6</button>
+                <button className="numpad-button symbol" onClick={handleOperator} type='button'>-</button>
+                <button className="numpad-button" onClick={handleClick} type='button'>1</button>
+                <button className="numpad-button" onClick={handleClick} type='button'>2</button>
+                <button className="numpad-button" onClick={handleClick} type='button'>3</button>
+                <button className="numpad-button symbol" onClick={handleOperator} type='button'>+</button>
+                <button className="numpad-button" id="zero" onClick={handleClick} type='button'>0</button>
+                <button className="numpad-button symbol" onClick={handleOperator} type='button'>.</button>
+                <button className="numpad-button symbol" onClick={handleOperator} type='button'>=</button>
             </form>
         </>
         )
--- calculator-app/src/components/Screen.jsx
+++ calculator-app/src/components/Screen.jsx
@@ -1,13 +1,65 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import './Screen.css'
 
-function Screen() {
+function Screen({input}) {
+    const [calculation, setCalculation] = useState({
+        number: '',
+        operator: '',
+        total: ''
+    })
+
+    useEffect(() => {
+        calculate()
+        reset()
+    },[input])
+
+    function calculate() {
+        const num = input.number
+        const oper = input.operator
+
+        if (oper === '÷') {
+            setCalculation({
+                number: num,
+                operator: oper,
+                total: Number(num.slice(0, -3))
+            })
+        } else if (oper === '+') {
+            setCalculation({
+                number: num,
+                operator: oper,
+                total: Number(num.slice(0, -3))
+            })
+        } else if (oper === '-') {
+            setCalculation({
+                number: num,
+                operator: oper,
+                total: Number(num.slice(0, -3))
+            })
+        } else if (oper === '+') {
+            setCalculation({
+                number: num,
+                operator: oper,
+                total: Number(num.slice(0, -3))
+            })
+        }
+    }
+
+    function reset() {
+        if (input.reset) {
+            setCalculation({
+                number: '',
+                operator: '',
+                total: ''
+            })
+        }
+    }
+
     return (
         <section id="screen">
             <div id="running-calc">
-                <h3>123456 + 6789 = 130245</h3>              
+                <h3>{calculation.number}{calculation.total}</h3>              
             </div>
-            <div id="current-input"><h2>123456</h2></div>
+            <div id="current-input"><h2>{input.number}</h2></div>
         </section>
     )
 }
